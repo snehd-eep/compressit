cde6e9ef70aa7bd8a8c8a34dde0e1d8c9006f66b
snehd-eep <53041298+snehd-eep@users.noreply.github.com>
2025-01-10 23:23:39 +0530

    Added cors

--- main.go
+++ main.go
@@ -4,14 +4,23 @@ import (
 	"log"
 	"net/http"
 	"testproject/routes"
+	"github.com/rs/cors"
 )
 
 func main() {
 	// Initialize routes
 	r := routes.InitializeRoutes()
 
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, // Allow all origins (adjust this for production)
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Allowed methods
+		AllowedHeaders: []string{"Content-Type", "Authorization", "X-Requested-With"}, // Allowed headers
+	})
+
+	r = corsHandler.Handler(r)
+
 	// Start the HTTPS server
-	log.Println("Starting HTTPS server on :443")
+	log.Println("Starting HTTPS server on :7070")
 	err := http.ListenAndServeTLS(
 		":7070",
 		"/etc/letsencrypt/live/34.123.178.159.nip.io/fullchain.pem",
