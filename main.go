e966afcd74e50e880bceb164830dc8e73dbbaba0
snehd-eep <53041298+snehd-eep@users.noreply.github.com>
2025-01-10 22:55:46 +0530

    Update main.go
    Added certificates

--- main.go
+++ main.go
@@ -10,9 +10,15 @@ import (
 func main() {
 
 	r := routes.InitializeRoutes()
-	err := r.Run(":7070")
+	log.Println("Starting HTTPS server on :443")
+	err := http.ListenAndServeTLS(
+		":443", 
+		"/etc/letsencrypt/live/34.123.178.159.nip.io/fullchain.pem", 
+		"/etc/letsencrypt/live/34.123.178.159.nip.io/privkey.pem", 
+		nil,
+	)
 	if err != nil {
-		return
+		log.Fatal("ListenAndServeTLS: ", err)
 	}
 }
 
