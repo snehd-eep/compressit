61fc6c79056aec3b1a30145fb553c9978ef6628c
snehd-eep <53041298+snehd-eep@users.noreply.github.com>
2025-01-10 23:00:40 +0530

    Update main.go
    errors resolved

--- main.go
+++ main.go
@@ -1,26 +1,24 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"testproject/routes"
 )
 
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
 func main() {
-
+	// Initialize routes
 	r := routes.InitializeRoutes()
+
+	// Start the HTTPS server
 	log.Println("Starting HTTPS server on :443")
 	err := http.ListenAndServeTLS(
-		":443", 
-		"/etc/letsencrypt/live/34.123.178.159.nip.io/fullchain.pem", 
-		"/etc/letsencrypt/live/34.123.178.159.nip.io/privkey.pem", 
-		nil,
+		":443",
+		"/etc/letsencrypt/live/34.123.178.159.nip.io/fullchain.pem",
+		"/etc/letsencrypt/live/34.123.178.159.nip.io/privkey.pem",
+		r, // Pass the router as the handler
 	)
 	if err != nil {
 		log.Fatal("ListenAndServeTLS: ", err)
 	}
 }
-
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
