d445498b0998be90ae7f6683c844c675ca57532b
snehd-eep <53041298+snehd-eep@users.noreply.github.com>
2025-01-11 00:00:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,13 +11,14 @@ func main() {
 	r := routes.InitializeRoutes()
 
 	// Enable CORS using Gin's built-in middleware
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"chrome-extension://emamohiiklpnloihcbnnbnbphfkahmnd", "https://yourdomain.com"}, // Allow specific origins (your Chrome extension and your domain)
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"}, // Allow specific methods
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Allow specific headers
-		ExposeHeaders:    []string{"Content-Length"}, // Expose response headers
-		AllowCredentials: true, // Allow cookies/credentials in requests if needed
-	})) // This applies default CORS settings, allowing all origins
+    	// Enable CORS using Gin's built-in middleware
+   	 r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+	    	"*", // Allow all origins (use for testing)
+	    	"https://34.123.178.159.nip.io",  // Your API domain
+	    	"chrome-extension://emamohiiklpnloihcbnnbnbphfkahmnd", // Your Chrome extension ID
+		},
+    	})) // This applies default CORS settings, allowing all origins
 
 	// Start the HTTPS server with TLS
 	log.Println("Starting HTTPS server on :7070")
